a61a9b589d0a568ec60eb81175a7c1efbb88ace6
Angel <archenyel@gmail.com>
2025-10-07 18:41:13 -0600

    cambios

---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+const hash = require('../index');
+
+test('Prueba unitaria de hash', () => {
+    const message = 'Hola Mundo';
+    const hash = hash(message);
+    expect(hash).toEqual('872e4bdc3f3f2b1e6f8b4d5c6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f');
+});
